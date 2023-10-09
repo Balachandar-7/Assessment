1d445a458cbe8e23490cd0cf675613d69c2b35ea
Apple <apple@Apples-MacBook-Pro.local>
2023-10-09 10:06:43 +0530

    home model update

--- source/models/home.js
+++ source/models/home.js
@@ -1,13 +1,6 @@
-// export default class HomeModel {
-//     constructor(json) {
-//       //console.log("Homeeee model", json);
-//       this.listData = json?.body
-//       //this.banner_images = json?.banner_images;
-
-//     }
-//   }
-
-const HomeModel = (props) => {
-  console.log("Homeeee model", props);
+export default class HomeModel {
+    constructor(json) {
+      //console.log("Homeeee model", json);
+      this.listData = json?.body
+    }
   }
-export default HomeModel;
